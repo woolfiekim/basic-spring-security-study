641930d75eff3b3c5c3435f63e4f06e44d4fa693
김주희 <woolfie@naver.com>
2023-05-23 17:56:04 +0900

    .

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -6,47 +6,30 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Order(0)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain adminFilterChain(HttpSecurity http) throws Exception {
 
-        http.securityMatcher("/admin/**");
 
         http
             .authorizeHttpRequests(
                 requests ->
                     requests
-                        // .requestMatchers("/admin/**").permitAll()
-                        // .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
             );
 
         http
-            .httpBasic();
+            .formLogin();
+
+        //이러면 자식 스레드와 공유하기 때문에 값이 있다.
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
 
         return http.build();
     }
 
-}
-
-@Configuration
-@Order(1)
-class SecurityConfig2 {
-
-    @Bean
-    public SecurityFilterChain globalFilterChain2(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(auth ->
-                auth.anyRequest().permitAll()
-            );
-
-        http.formLogin(Customizer.withDefaults());
-
-        return http.build();
-    }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,42 +1,44 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(){
-       return "home";
-    }
-    @GetMapping("loginPage")
-    public String loginPage(){
-        return "loginPage";
+    public String index(HttpSession session){
+
+        //인증객체1 (SecurityContextHolder 사용)
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        //인증객체2 (Session 사용)
+        SecurityContext context = (SecurityContext)session.getAttribute(
+            HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
+
+        /*
+         * 인증객체1과 2는 동일한 객체이다.
+         */
+
+        return "home";
     }
 
-    @GetMapping("/user")
-    public String user(){
-        return "user";
-    }
+    @GetMapping("/thread")
+    public String thread(){
+        //자식 스레드이기 때문에 값이 null이다.
+        new Thread(
+            () -> {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            }
+        ).start();
 
-    @GetMapping("/admin/pay")
-    public String adminPay(){
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin(){
-        return "admin";
-    }
-
-    @GetMapping("/denied")
-    public String denied(){
-        return "Access is denied";
-    }
-
-    @GetMapping("/login")
-    public String login(){
-        return "login ";
+        return "thread";
     }
 }
